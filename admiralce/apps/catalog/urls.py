d81e6c425f2c43a300bda43cf6b99f17603f049f
Burton Johnsey <git@tonjohn.com>
2017-11-14 22:29:10 -0800

    Hooked up list view and detail view for courses

--- admiralce/apps/catalog/urls.py
+++ admiralce/apps/catalog/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?P<slug>[-\w\d]+)/$', views.view_sitter, name='profile')
+    # url(r'^(?P<slug>[-\w\d]+)/$', views.view_sitter, name='profile'),
+    url(r'^courses/$', views.CourseListView.as_view(), name='course-list'),
+    url(r'^course/(?P<pk>\d+)$', views.CourseDetailView.as_view(), name='course-detail'),
 ]
--- admiralce/apps/catalog/views.py
+++ admiralce/apps/catalog/views.py
@@ -1,9 +1,26 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Sitter
+from .models import Course, Provider
+
+from django.views import generic
+
+
+class CourseListView(generic.ListView):
+    model = Course
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super(CourseListView, self).get_context_data(**kwargs)
+        # Get the blog from id and add it to the context
+        context['provider_list'] = Provider.objects.all()
+        return context
+
+
+class CourseDetailView(generic.DetailView):
+    model = Course
 
 
 def view_sitter(request, slug):
-    sitter = get_object_or_404(Sitter, url=slug)
+    sitter = get_object_or_404(Course, url=slug)
     context = {
         'sitter': sitter
     }
